7e0362a4cd8af1399d99c54962761f3acc0c195d
simolasa <simone.lasagni@studio.unibo.it>
2023-02-23 16:47:03 +0100

    adding unity project

---
+++ Assets/left_racket_script.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections.LowLevel.Unsafe;
+using UnityEngine;
+
+public class left_racket_script : MonoBehaviour
+{
+    void Start()
+    {
+  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.W) == true)
+        {
+
+        }
+    }
+}
